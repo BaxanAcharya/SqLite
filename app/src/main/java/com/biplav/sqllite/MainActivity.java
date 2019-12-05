0e658053fc47d5ba5af0d921ff098378ad7db9b9
BaxanAcharya <baxanacharya@gmail.com>
2019-12-05 11:33:05 +0545

    Initial commit

---
+++ app/src/main/java/com/biplav/sqllite/MainActivity.java
@@ -0,0 +1,29 @@
+package com.biplav.sqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etword,etMeaning;
+    Button btnaddWord;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etword=findViewById(R.id.etWord);
+        etMeaning=findViewById(R.id.etMeaning);
+        btnaddWord=findViewById(R.id.btnAdd);
+        btnaddWord.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
